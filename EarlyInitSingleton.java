4076afa76d855078684e28835405d1ed506c6137
janne131058 <pengjiaous0812@gmail.com>
2022-04-13 22:52:29 -0700

    add double check

---
+++ EarlyInitSingleton.java
@@ -0,0 +1,6 @@
+public class EarlyInitSingleton {
+    private static final EarlyInitSingleton INSTANCE = new EarlyInitSingleton();
+    public static EarlyInitSingleton getInstance() {
+        return INSTANCE;
+    }
+}
---
+++ InitOnDemandSingleton.java
@@ -0,0 +1,8 @@
+public class InitOnDemandSingleton {
+    private static class InstanceHolder {
+        private static final InitOnDemandSingleton INSTANCE = new InitOnDemandSingleton();
+    }
+    public static InitOnDemandSingleton getInstance() {
+        return InstanceHolder.INSTANCE;
+    }
+}
