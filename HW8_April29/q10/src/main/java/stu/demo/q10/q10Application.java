435397b06d5e5355da4f6064d9875f9d72106164
AxcelXander <tyz666@bu.edu>
2022-04-29 18:36:16 -0700

    Homework 8, April 29

---
+++ HW8_April29/q10/src/main/java/stu/demo/q10/q10Application.java
@@ -0,0 +1,13 @@
+package stu.demo.q10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class q10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(q10Application.class, args);
+    }
+
+}
